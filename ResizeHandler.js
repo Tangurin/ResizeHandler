d2efe63ac19d1dd7bec9436fc3fe24b9efd6462e
Albert <webbhelp_@hotmail.com>
2016-11-16 16:27:01 +0100

    Improvements

--- ResizeHandler.js
+++ ResizeHandler.js
@@ -1,18 +1,18 @@
 var ResizeHandler = {
     active: false,
     element: null,
-    scrollEvent: null,
     onStopTimer: null,
-    options: {},
-    initialize: function($element, options) {
+    callbacks: {
+        onResize: [],
+        onResizeStop: [],
+    },
+    initialize: function() {
         if (ResizeHandler.active == true) {
             return true;
         }
-        var options = options || {};
-        options = $.extend(ResizeHandler.options,  options);
 
-        ResizeHandler.element = $element;
-        ResizeHandler.scrollEvent = ResizeHandler.element.on('resize', ResizeHandler.onResize);
+        ResizeHandler.element = $(window);
+        ResizeHandler.element.on('resize', ResizeHandler.onResize);
         ResizeHandler.active = true;
     },
     onResize: function() {
@@ -20,8 +20,23 @@ var ResizeHandler = {
         //On Resize Stop
         clearTimeout(ResizeHandler.onStopTimer);
         ResizeHandler.onStopTimer = setTimeout(ResizeHandler.onResizeStop, 300);
+
+        var callbacks = ResizeHandler.callbacks.onResize;
+        for (var i in callbacks) {
+            callbacks[i](ResizeHandler.element);
+        }
     },
     onResizeStop: function() {
         ResizeHandler.element.trigger('ResizeHandler-ResizeStop');
+        var callbacks = ResizeHandler.callbacks.onResizeStop;
+        for (var i in callbacks) {
+            callbacks[i](ResizeHandler.element);
+        }
+    },
+    callback: function(callback, type) {
+        var type = type || 'onResize';
+        if (typeof callback == 'function' && typeof ResizeHandler.callbacks[type] != 'undefined') {
+            ResizeHandler.callbacks[type].push(callback);
+        }
     }
 };
