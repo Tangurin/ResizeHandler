5e7dda05070636e84bd1d781761a4b238726421f
Tobias Olsson <firewolf882002@hotmail.com>
2016-11-23 11:08:39 +0100

    Export module for CommonJS and AMD

--- ResizeHandler.js
+++ ResizeHandler.js
@@ -1,52 +1,74 @@
-var ResizeHandler = {
-    active: false,
-    element: null,
-    onEndWaitTime: 500,
-    onEndTimer: null,
-    callbacks: {
-        onResize: [],
-        onResizeEnd: [],
-    },
-    initialize: function() {
-        if (ResizeHandler.active == true) {
-            return true;
-        }
-
-        ResizeHandler.element = $(window);
-        ResizeHandler.element.on('resize', ResizeHandler.triggered);
-        ResizeHandler.active = true;
-    },
-    triggered: function(event) {
-        var $this = $(this)
-        var callbacks = ResizeHandler.callbacks;
-
-        //onResize
-        for (var i in callbacks.onResize) {
-            callbacks.onResize[i]($this, event);
-        }
-
-        //onResizeEnd
-        clearTimeout(ResizeHandler.onEndTimer);
-        ResizeHandler.onEndTimer = setTimeout(function() {
-            for (var i in callbacks.onResizeEnd) {
-                callbacks.onResizeEnd[i]($this, event);
+(function () {
+    'use strict';
+    /*===========================
+    ResizeHandler
+    ===========================*/
+    var ResizeHandler = {
+        active: false,
+        element: null,
+        onEndWaitTime: 500,
+        onEndTimer: null,
+        callbacks: {
+            onResize: [],
+            onResizeEnd: [],
+        },
+        initialize: function() {
+            if (ResizeHandler.active == true) {
+                return true;
             }
-        }, ResizeHandler.onEndWaitTime);
-    },
-    onResize: function(callback) {
-        if (typeof callback == 'function') {
-            ResizeHandler.callbacks.onResize.push(callback);
+
+            ResizeHandler.element = $(window);
+            ResizeHandler.element.on('resize', ResizeHandler.triggered);
+            ResizeHandler.active = true;
+        },
+        triggered: function(event) {
+            var $this = $(this)
+            var callbacks = ResizeHandler.callbacks;
+
+            //onResize
+            for (var i in callbacks.onResize) {
+                callbacks.onResize[i]($this, event);
+            }
+
+            //onResizeEnd
+            clearTimeout(ResizeHandler.onEndTimer);
+            ResizeHandler.onEndTimer = setTimeout(function() {
+                for (var i in callbacks.onResizeEnd) {
+                    callbacks.onResizeEnd[i]($this, event);
+                }
+            }, ResizeHandler.onEndWaitTime);
+        },
+        onResize: function(callback) {
+            if (typeof callback == 'function') {
+                ResizeHandler.callbacks.onResize.push(callback);
+            }
+        },
+        onResizeEnd: function(callback) {
+            if (typeof callback == 'function') {
+                ResizeHandler.callbacks.onResizeEnd.push(callback);
+            }
+        },
+        callback: function(callback, type) {
+            var type = type || 'onResize';
+            if (typeof callback == 'function' && typeof ResizeHandler.callbacks[type] != 'undefined') {
+                ResizeHandler.callbacks[type].push(callback);
+            }
         }
-    },
-    onResizeEnd: function(callback) {
-        if (typeof callback == 'function') {
-            ResizeHandler.callbacks.onResizeEnd.push(callback);
-        }
-    },
-    callback: function(callback, type) {
-        var type = type || 'onResize';
-        if (typeof callback == 'function' && typeof ResizeHandler.callbacks[type] != 'undefined') {
-            ResizeHandler.callbacks[type].push(callback);
-        }
-    }
-};
+    };
+
+    window.ResizeHandler = ResizeHandler;
+})();
+
+/*===========================
+ResizeHandler AMD Export
+===========================*/
+if (typeof(module) !== 'undefined')
+{
+    module.exports = window.ResizeHandler;
+}
+else if (typeof define === 'function' && define.amd) {
+    define([], function () {
+        'use strict';
+        return window.ResizeHandler;
+    });
+}
